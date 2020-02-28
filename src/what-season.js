39cd63185bb5aafeacba6fda3669d5a44c0b4c1c
Ange <genachumakov@mail.ru>
2020-02-28 11:40:21 +0300

    fix: solution tricky moment

--- src/what-season.js
+++ src/what-season.js
@@ -1,10 +1,14 @@
 module.exports = function getSeason(date) {
-  if (!date) return 'Unable to determine the time of year!';
+  if (date === undefined) {
+    return 'Unable to determine the time of year!';
+  }
+  if (!date || !date.getMilliseconds()){
+    throw 'error';
+  }
   const month = date.getMonth();
   if (month >= 0 & month < 2 || month == 11) return 'winter';
   else if (month >= 2 & month < 5) return 'spring';
   else if (month >= 5 & month < 8) return 'summer';
   else if (month >= 8 & month < 11) return 'autumn';
   else return 'Unable to determine the time of year!';
-
 };
